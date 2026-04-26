102e264dbc888e06f4f3002782c34ab1dde77d79
JoannaCCJH <joannacccc@163.com>
2026-04-26 19:38:11 -0400

    demo to run cli tools

---
+++ backend/demo.sh
@@ -0,0 +1,11 @@
+python backend/scripts/utilities/password_generator.py random 4
+
+python backend/scripts/utilities/unit_converter.py convert -273.15 celsius kelvin temperature
+
+python backend/scripts/utilities/unit_converter.py convert 1000 m km length
+
+python backend/scripts/utilities/age_calculator.py age 1995-06-15
+
+python backend/scripts/utilities/age_calculator.py age 2000-02-29 2021-03-01
+
+python backend/scripts/utilities/age_calculator.py milestones 1995-06-15
